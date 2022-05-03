32448c7daaea0b09bdc6074ce025fd0382fb84e5
patrickd77-eng <patrick_Davis97@hotmail.co.uk>
2022-05-03 19:11:52 +0100

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using MaxDoseCheckerMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace MaxDoseCheckerMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+           var drugList = Drugs.GetDrugInfoFromCsv();
+
+            return View(drugList);
+        }
+
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Drugs.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace MaxDoseCheckerMVC.Models
+{
+    public class Drugs
+
+    {
+        public int Id { get; set; }
+        public string Route { get; set; }
+        public string Name { get; set; }
+         public decimal MaxDose { get; set; }
+         public string DoseType { get; set; }
+        public static List<Drugs> GetDrugInfoFromCsv()
+        {
+
+
+            string path = System.IO.Directory.GetCurrentDirectory()
+            + @"\data\drugs.csv";
+
+            List<Drugs> values = File.ReadAllLines(path)
+                                           .Skip(1)
+                                           .Select(v => Drugs.FromCsv(v))
+                                           .ToList();
+
+            return values;
+
+        }
+
+        public static Drugs FromCsv(string csvLine)
+        {
+            string[] values = csvLine.Split(',');
+            Drugs Drugs = new Drugs();
+            Drugs.Id = Convert.ToInt32(values[0]);
+            Drugs.Route = Convert.ToString(values[1]);
+            Drugs.Name = Convert.ToString(values[2]);
+            Drugs.MaxDose = Convert.ToDecimal(values[3]);
+            Drugs.DoseType = Convert.ToString(values[4]);
+
+            return Drugs;
+        }
+    }
+}
